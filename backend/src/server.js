ca55f413e89ba68c92432840158058d313138859
aniket190705 <aniket190705@gmail.com>
2025-09-03 08:09:13 +0530

    cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://notes-app-qrpz.onrender.com", credentials: true }));
 
 console.log("MongoDB URI:", MONGO_URI);
 mongoose.connect(MONGO_URI)
